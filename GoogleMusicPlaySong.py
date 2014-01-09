0476c6fd754d0baf47674d95b7630c01acaaf185
foreverguest <leo.cervo@ibest.com.br>
2014-01-09 19:02:45 -0200

    Improve url prefetch
    Should work in more cenarios
    Track item creation improved

--- GoogleMusicPlaySong.py
+++ GoogleMusicPlaySong.py
@@ -1,21 +1,17 @@
 import sys, time
-import simplejson as json
 
 class GoogleMusicPlaySong():
 
     def __init__(self):
         self.main       = sys.modules["__main__"]
-        self.xbmcplugin = self.main.xbmcplugin
         self.xbmcgui    = self.main.xbmcgui
-        self.xbmc       = self.main.xbmc
-        self.storage    = self.main.storage
-        self.prefetch   = self.main.settings.getSetting( "prefetch" )
 
     def play(self, song_id, params={}):
-        song = self.storage.getSong(song_id)
+        song = self.main.storage.getSong(song_id)
+        prefetch = self.main.settings.getSetting( "prefetch" )
 
         if song:
-            if self.prefetch=="false" or not song[24] or int(self.main.parameters_string_to_dict(song[24]).get('expire'))  < time.time():
+            if prefetch=="false" or not song[24] or int(self.main.parameters_string_to_dict(song[24]).get('expire'))  < time.time():
                  self.main.log("Prefetch disabled or URL invalid or expired :")
                  url = self.__getSongStreamUrl(song_id)
             else:
@@ -31,42 +27,17 @@ class GoogleMusicPlaySong():
             li.setProperty('Music', 'true')
             url = self.__getSongStreamUrl(song_id)
 
-
         self.main.log("URL :: "+repr(url))
 
         li.setPath(url)
-        self.xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=li)
+        self.main.xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=li)
 
-        if self.prefetch=="true":
-         try:
-            #wait for song playing and playlist ready
-            self.xbmc.sleep(10000)
-            # get song position in playlist
-            get_players = json.loads(self.xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Player.GetProperties", "params": {"playerid":0,"properties":["playlistid","position"]},"id": 1}'))
-            position = get_players['result']['position']
-
-            # get next song id and fetch url
-            get_playlist = json.loads(self.xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Playlist.GetItems", "params": {"playlistid":0, "properties": ["file","duration"]}, "id": 1}'))
-            self.main.log("Get playlist:: "+repr(get_playlist))
-
-            song_id_next = self.main.parameters_string_to_dict(get_playlist['result']['items'][position+1]['file']).get("song_id")
-            self.__getSongStreamUrl(song_id_next)
-
-            # get playing song duration
-            duration = get_playlist['result']['items'][position]['duration']
-            # stream url expires in 1 minute, refetch to always have a valid one
-            while duration > 50:
-                self.xbmc.sleep(50000)
-                duration = duration - 50
-                # test if user manually changed the music
-                get_players = json.loads(self.xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Player.GetProperties", "params": {"playerid":0,"properties":["playlistid","position"]},"id": 1}'))
-                if position != get_players['result']['position']:
-                    break
-                # before the stream url expires we fetch it again
-                self.__getSongStreamUrl(song_id_next)
-         except Exception as ex:
-            self.main.log("ERROR trying to fetch url: "+repr(ex))
-            #raise
+        if prefetch=="true":
+            try:
+                self.__prefetchUrl()
+            except Exception as ex:
+                self.main.log("ERROR trying to fetch url: "+repr(ex))
+                #raise
 
     def __getSongStreamUrl(self,song_id):
         import GoogleMusicApi
@@ -98,3 +69,53 @@ class GoogleMusicPlaySong():
         li.setInfo(type='music', infoLabels=infoLabels)
 
         return li
+
+    def __prefetchUrl(self):
+        import gmusicapi.compat as compat
+        loadJson = compat.json.loads
+        xbmc     = self.main.xbmc
+        jsonGetPlaylistPos  = '{"jsonrpc":"2.0", "method":"Player.GetProperties", "params":{"playerid":0,"properties":["playlistid","position","percentage"]},"id":1}'
+        jsonGetSongDuration = '{"jsonrpc":"2.0", "method":"Playlist.GetItems",    "params":{"playlistid":0, "properties":["file","duration"]}, "id":1}'
+
+        # get song position in playlist
+        playerProperties = loadJson(xbmc.executeJSONRPC(jsonGetPlaylistPos))
+        while not 'result' in playerProperties:
+          #wait for song playing and playlist ready
+          xbmc.sleep(1000)
+          playerProperties = loadJson(xbmc.executeJSONRPC(jsonGetPlaylistPos))
+
+        while playerProperties['result']['percentage'] > 5:
+          #wait for new song playing
+          xbmc.sleep(1000)
+          playerProperties = loadJson(xbmc.executeJSONRPC(jsonGetPlaylistPos))
+
+        position = playerProperties['result']['position']
+        self.main.log("position:"+str(position)+" percentage:"+str(playerProperties['result']['percentage']))
+
+        # get next song id and fetch url
+        playlistItems = loadJson(xbmc.executeJSONRPC(jsonGetSongDuration))
+        #self.main.log("playlistItems:: "+repr(playlistItems))
+
+        if position+1 >= len(playlistItems['result']['items']):
+            self.main.log("playlist end:: position "+repr(position)+" size "+len(playlistItems['result']['items']))
+            return
+
+        song_id_next = self.main.parameters_string_to_dict(playlistItems['result']['items'][position+1]['file']).get("song_id")
+        self.__getSongStreamUrl(song_id_next)
+
+        # get playing song duration
+        duration = playlistItems['result']['items'][position]['duration']
+
+        # stream url expires in 1 minute, refetch to always have a valid one
+        while duration > 50:
+            xbmc.sleep(50000)
+            duration = duration - 50
+
+            # test if music changed
+            playerProperties = loadJson(xbmc.executeJSONRPC(jsonGetPlaylistPos))
+            if not 'result' in playerProperties or position != playerProperties['result']['position']:
+                self.main.log("ending:: position "+repr(position)+" "+repr(playerProperties))
+                break
+
+            # before the stream url expires we fetch it again
+            self.__getSongStreamUrl(song_id_next)
