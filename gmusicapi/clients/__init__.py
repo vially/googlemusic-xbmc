4970d26ef7bcd4f8e56ff948370c8ad06b630590
Leonardo Cervo <leo.cervo@ibest.com.br>
2015-07-01 19:13:29 -0300

    Remove gmusicapi module
    Users should provide a compatible gmusicapi module

--- gmusicapi/clients/__init__.py
+++
@@ -1,5 +0,0 @@
-#from gmusicapi.clients.webclient import Webclient
-#from gmusicapi.clients.musicmanager import Musicmanager, OAUTH_FILEPATH
-from gmusicapi.clients.mobileclient import Mobileclient
-
-#(Webclient, Musicmanager, Mobileclient, OAUTH_FILEPATH)  # noqa
